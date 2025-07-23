26f1f9dc239a8ea009e63201974806e388107a8e
pawan-kavinda <pawankavinda09@gmail.com>
2025-07-23 21:22:02 +0530

    logs

--- Voter.java
+++ Voter.java
@@ -1,23 +1,54 @@
 import javax.crypto.SecretKey;
 import java.security.KeyPair;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.util.Base64;
 
 public class Voter {
     public KeyPair keyPair;
-
-    public Voter() throws Exception {
-        keyPair = Utils.generateRSAKeyPair();
+   
+    public boolean verifyServerResponse(String serverVerificationCode, String signedServerVerificationCode, PublicKey serverPubKey) throws Exception {
+       System.out.println("Step 1 >>> Alice verifies server response...");
+       System.out.println("");
+       System.out.println("Server Verification Code: " + serverVerificationCode);
+       System.out.println("");
+       System.out.println("Signed Server Verification Code: " + signedServerVerificationCode);
+       System.out.println("");
+       System.out.println("Server Public Key: " + serverPubKey);
+       System.out.println("");
+       return Utils.verify(serverVerificationCode, signedServerVerificationCode, serverPubKey);
     }
 
-    public VotePackage prepareVote(String vote, PublicKey serverPubKey) throws Exception {
-        String hash = Utils.hashSHA256(vote);
-        String signature = Utils.sign(hash, keyPair.getPrivate());
+    public String generateServerVerificationCode() {
+        byte[] bytes = new byte[16];
+        new SecureRandom().nextBytes(bytes);
+        return Base64.getEncoder().encodeToString(bytes);
+    }
 
+    public VotePackage prepareAnonymousVote(String vote, PublicKey serverPubKey, Authority authority) throws Exception {
         SecretKey aesKey = Utils.generateAESKey();
         String encryptedVote = Utils.encryptAES(vote, aesKey);
         String encryptedAESKey = Utils.encryptRSA(Base64.getEncoder().encodeToString(aesKey.getEncoded()), serverPubKey);
 
-        return new VotePackage(encryptedVote, encryptedAESKey, signature, keyPair.getPublic(), hash);
+        String nonce = Utils.generateNonce();
+        String token = Utils.hashSHA256(vote + nonce);
+        String signedToken = authority.signToken(token);
+
+        VotePackage pkg = new VotePackage(encryptedVote, encryptedAESKey, signedToken, token, nonce);        
+        System.out.println("Step 2 >>> Alice sends vote package");
+        System.out.println("");
+        System.out.println("Encrypted Vote: " + pkg.encryptedVote);
+        System.out.println("");
+        System.out.println("Encrypted AES Key: " + pkg.encryptedAESKey);
+        System.out.println("");
+        System.out.println("Signed Token: " + pkg.signedToken);
+        System.out.println("");
+        System.out.println("Token: " + pkg.token);
+        System.out.println("");
+        System.out.println("Nonce: " + pkg.nonce);
+        System.out.println("");
+
+        
+        return pkg;
     }
 }
